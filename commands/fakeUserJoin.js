6baeb021308f78dbd148ae8aff866569176e66bc
David Moll <david.leander.moll@gmail.com>
2021-08-20 17:19:38 +0200

    Removed commands

--- commands/fakeUserJoin.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-
-module.exports = {
-	name: `fakeuserjoin`,
-	execute(client, message, args) {
-		message.client.emit(`guildMemberAdd`, message, message.member);
-	},
-};
