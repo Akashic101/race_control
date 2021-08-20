53ca4f5ba9b36eaf4b80350dea0a6df1e773f244
David Moll <david.leander.moll@gmail.com>
2021-08-20 17:19:06 +0200

    Added message-caching

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,9 @@
 /* eslint-disable no-undef */
 
 module.exports = (client) => {
+	client.channels.cache.get(`849546394208829470`).messages.fetch(`877228914467045478`).then(m => {
+        console.log("Cached game-roles message.");
+    })
+
 	console.log(`ready`);
 };
